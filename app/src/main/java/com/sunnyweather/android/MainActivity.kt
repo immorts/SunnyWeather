c08d1b66f8beb8aae1d1f11bfe36c9f9ae44b7f9
immorts <3197166390@qq.com>
2021-10-11 16:00:21 +0800

    SunnyWeather App 开发

--- app/src/main/java/com/sunnyweather/android/MainActivity.kt
+++ app/src/main/java/com/sunnyweather/android/MainActivity.kt
@@ -7,10 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        initView()
     }
 
-    private fun initView() {
-
-    }
 }
