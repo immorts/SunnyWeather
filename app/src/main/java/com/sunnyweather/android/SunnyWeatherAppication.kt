f1d55ba1c57889eeb810aa77139b7583adb31463
immorts <3197166390@qq.com>
2021-10-11 16:16:28 +0800

    SunnyWeather App 开发

---
+++ app/src/main/java/com/sunnyweather/android/SunnyWeatherAppication.kt
@@ -0,0 +1,15 @@
+package com.sunnyweather.android
+
+import android.app.Application
+
+class SunnyWeatherAppication : Application(){
+
+
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+
+
+}
